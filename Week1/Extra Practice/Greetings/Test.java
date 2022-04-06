9991fbc441ca73a2e2bbe5e2a9e2ef64043199c7
ldozoria <dianaozoria@gmail.com>
2022-04-06 14:36:14 -0400

    First commit

---
+++ Week1/Extra Practice/Greetings/Test.java
@@ -0,0 +1,7 @@
+public class Test { // file name and class name must be the same in order to run this
+    // class should be written Pascal case (start w/ uppercase, and each add word needs a capital start)
+    public static void main(String[] args) {
+        // all code will go here 
+        System.out.println("Hello World!");
+    }
+}
