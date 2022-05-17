f2fdb73a7d616f9c3a2aa8e37ff56908ac2758da
ldozoria <97138607+ldozoria@users.noreply.github.com>
2022-05-17 14:00:35 -0400

    Update Book.java

--- Week5/Book Club/bookClub/src/main/java/com/diana/bookclub/models/Book.java
+++ Week5/Book Club/bookClub/src/main/java/com/diana/bookclub/models/Book.java
@@ -11,11 +11,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-//import javax.persistence.PrePersist;
-//import javax.persistence.PreUpdate;
+
 import javax.persistence.Table;
-//import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name="books")
@@ -24,14 +24,22 @@ public class Book {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
-	@NotEmpty(message="Every book need a title!")
+	
+	@NotEmpty(message="Book title cannot be left blank!")
 	private String title;
-	@NotEmpty(message="Don't forget the author!")
+	
+	@NotEmpty(message="Author field cannot be left blank!")
 	private String author;
+	
+	@NotEmpty(message="Comment field cannot be left blank!")
 	private String comment;
+
 	@Column(updatable=false)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createdAt;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date updatedAt;
+	
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="postedBy")
 	private User user;
@@ -45,61 +53,60 @@ public class Book {
 		this.user = user;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
-
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
+		public Long getId() {
+			return id;
+		}
 	
+		public void setId(Long id) {
+			this.id = id;
+		}
 	
-}
+		public String getTitle() {
+			return title;
+		}
+	
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	
+		public String getAuthor() {
+			return author;
+		}
+	
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+	
+		public String getComment() {
+			return comment;
+		}
+	
+		public void setComment(String comment) {
+			this.comment = comment;
+		}
+	
+		public Date getCreatedAt() {
+			return createdAt;
+		}
+	
+		public void setCreatedAt(Date createdAt) {
+			this.createdAt = createdAt;
+		}
+	
+		public Date getUpdatedAt() {
+			return updatedAt;
+		}
+	
+		public void setUpdatedAt(Date updatedAt) {
+			this.updatedAt = updatedAt;
+		}
+	
+		public User getUser() {
+			return user;
+		}
+	
+		public void setUser(User user) {
+			this.user = user;
+		}
+		
+	}
